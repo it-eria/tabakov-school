72df10e59c44ec8263fc6405a2c7f94fbe8f83fb
Vitaliy <pyzhyck@gmail.com>
2018-05-15 08:56:32 +0300

    Update project files

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+  
+});
+
+function iteriaWatermark() {
+  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
+  console.log("            hMM                   yMM`            ");
+  console.log("            hMM      /oooooo`     yMM`            ");
+  console.log("            hMM      sdddddd`     yMM`            ");
+  console.log("            hMM      sdddddd`     yMM`            ");
+  console.log("            hMM      /oooooo`     yMM`            ");
+  console.log("            hMM                   yMM`            ");
+  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
+  console.log("                                                  ");
+  console.log(".` .::::::::` -oooooo.  ooo++:    :.      /.      ");
+  console.log("/-     :-     /+        m   `/m`  s:     /hd`     ");
+  console.log("/-     :-     /+        m    `m-  s:    -d`:h     "); 
+  console.log("/-     :-     /hoooo+   N+++yo-   s:   `m:``ys    ");
+  console.log("/-     :-     /+        m   oy`   s:   do++++h/   ");
+  console.log("/-     :-     /o        m    :d.  s:  yo     `m-  ");
+  console.log(".`     ``     .//////\`  :     .:  -` `/       .: ");
+}
+
+iteriaWatermark();
