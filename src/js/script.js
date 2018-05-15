a1320c96c0202848f2d88aa762d01b579db197c8
Vitaliy <pyzhyck@gmail.com>
2018-05-15 09:35:50 +0300

    Update src folder

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,40 @@
+// Burger script
 $(function() {
-  
+  $("#hamburger").on("click", function() {
+    $(this).toggleClass("is-open");
+    $(this).toggleClass("is-closed");
+    if ($(window).width() < 993 && $(this).hasClass("is-open")) {
+      $(".header__main-nav").show(300);
+    } else {
+      $(".header__main-nav").hide(300);
+    }
+  });
+
+  $('.burger-button').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('clicked');
+    $('.header__main-nav').fadeToggle(300);
+  });
+
+  $(window).on("resize", function() {
+    if ($(window).width() > 991) {
+      $(".header__main-nav").removeAttr('style');
+    }
+  });
 });
-
-function iteriaWatermark() {
-  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
-  console.log("            hMM                   yMM`            ");
-  console.log("            hMM      /oooooo`     yMM`            ");
-  console.log("            hMM      sdddddd`     yMM`            ");
-  console.log("            hMM      sdddddd`     yMM`            ");
-  console.log("            hMM      /oooooo`     yMM`            ");
-  console.log("            hMM                   yMM`            ");
-  console.log("            hMMMMMMMy       :MMMMMMMM`            ");
-  console.log("                                                  ");
-  console.log(".` .::::::::` -oooooo.  ooo++:    :.      /.      ");
-  console.log("/-     :-     /+        m   `/m`  s:     /hd`     ");
-  console.log("/-     :-     /+        m    `m-  s:    -d`:h     "); 
-  console.log("/-     :-     /hoooo+   N+++yo-   s:   `m:``ys    ");
-  console.log("/-     :-     /+        m   oy`   s:   do++++h/   ");
-  console.log("/-     :-     /o        m    :d.  s:  yo     `m-  ");
-  console.log(".`     ``     .//////\`  :     .:  -` `/       .: ");
+// Google Maps
+function initMap() {
+  var markerPos = {lat: 49.796177, lng: 24.055697};
+  var centerPos = {lat: markerPos.lat, lng: markerPos.lng - 0.001};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: centerPos,
+    disableDefaultUI: true
+  });
+  
+  var marker = new google.maps.Marker({
+    position: markerPos,
+    map: map,
+    title: 'Wohlfahrtswerk für Baden-Württemberg'
+  });
 }
-
-iteriaWatermark();
